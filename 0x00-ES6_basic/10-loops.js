059838906c0a3b9dbe1349890afa928792cf7f87
SafaFarah <122905189+SafaFarah@users.noreply.github.com>
2024-07-22 13:35:34 +0300

    Update 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (let value of array) {
-    let index = array.indexOf(value);
+    const index = array.indexOf(value);
     array[index] = appendString + value;
   }
   
