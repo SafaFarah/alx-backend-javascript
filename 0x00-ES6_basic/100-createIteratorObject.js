c3b4f79881427d7ad41246ff5806197e5388c48e
SafaFarah <kangjisook13@gmail.com>
2024-07-22 10:57:34 +0300

    create tasks 's files

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,8 @@
+export default function createIteratorObject(report) {
+  const employees = Object.values(report.allEmployees).flat();
+  return (function* () {
+    for (const employee of employees) {
+      yield employee;
+    }
+  })();
+}
