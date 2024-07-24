79e2563015b353fd7a9ce78188e2058612c312b2
SafaFarah <kangjisook13@gmail.com>
2024-07-24 10:32:51 +0300

    create 3-all.js

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoRes, userRes]) => {
+      console.log(`${photoRes.body} ${userRes.firstName} ${userRes.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
