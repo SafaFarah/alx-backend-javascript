157edb95e909bb9cd7236b7323330f42184d7238
SafaFarah <kangjisook13@gmail.com>
2024-07-26 15:34:25 +0300

    debug lint style

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -4,27 +4,33 @@ export default class HolbertonCourse {
     this.length = length;
     this.students = students;
   }
+
   get name() {
     return this._name;
   }
+
   get length() {
     return this._length;
   }
+
   get students() {
     return this._students;
   }
+
   set name(name) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = name;
   }
+
   set length(length) {
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = length;
   }
+
   set students(students) {
     if (students instanceof Array) {
       this._students = students;
