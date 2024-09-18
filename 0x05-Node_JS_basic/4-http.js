8c74790672989129f47c4e6596fc6234709d1be3
SafaFarah <kangjisook13@gmail.com>
2024-09-18 20:45:02 +0300

    debug 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,10 @@
 const http = require('http');
+
 const PORT = 1245;
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
