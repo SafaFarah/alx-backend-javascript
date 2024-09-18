6ef08bb9ab05f29bba15f585f69eb8b4537bf56d
SafaFarah <kangjisook13@gmail.com>
2024-09-18 20:23:03 +0300

    debug lint in 3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -4,15 +4,15 @@ async function countStudents(path) {
   try {
     await fs.access(path);
     const data = await fs.readFile(path, 'utf8');
-    const lines = data.split('\n').filter(line => line.trim() !== '');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
     const [headers, ...rows] = lines;
     const headerArray = headers.split(',');
     const firstNameIndex = headerArray.indexOf('firstname');
     const fieldIndex = headerArray.indexOf('field');
     const studentsByField = {};
     let totalStudents = 0;
-    rows.forEach(row => {
-      const columns = row.split(',').map(cell => cell.trim());
+    rows.forEach((row) => {
+      const columns = row.split(',').map((cell) => cell.trim());
       const firstName = columns[firstNameIndex];
       const field = columns[fieldIndex];
       if (firstName && field) {
