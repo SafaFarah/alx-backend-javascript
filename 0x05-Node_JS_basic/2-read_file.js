a958dc587654936f4ad23b83306a5cb137ca10cd
SafaFarah <kangjisook13@gmail.com>
2024-09-18 19:35:30 +0300

    create 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -16,15 +16,15 @@ function countStudents(path) {
   const studentsByField = {};
   let totalStudents = 0;
   rows.forEach(row => {
-  const columns = row.split(',').map(cell => cell.trim());
-  const firstName = columns[firstNameIndex];
-  const field = columns[fieldIndex];
-  if (firstName && field) {
-    totalStudents += 1;
-    if (!studentsByField[field]) {
-      studentsByField[field] = [];
-    }
-    studentsByField[field].push(firstName);
+    const columns = row.split(',').map(cell => cell.trim());
+    const firstName = columns[firstNameIndex];
+    const field = columns[fieldIndex];
+    if (firstName && field) {
+      totalStudents += 1;
+      if (!studentsByField[field]) {
+        studentsByField[field] = [];
+      }
+      studentsByField[field].push(firstName);
     }
   });
   console.log(`Number of students: ${totalStudents}`);
