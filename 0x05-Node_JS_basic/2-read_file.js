db4076b73b65668784609c6924ee62763fb7517a
SafaFarah <kangjisook13@gmail.com>
2024-09-18 20:19:20 +0300

    update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,9 +4,6 @@ function countStudents(path) {
   if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
   }
-  if (!fs.statSync(path).isFile()) {
-    throw new Error('Cannot load the database');
-  }
   const data = fs.readFileSync(path, 'utf8');
   const lines = data.split('\n').filter((line) => line.trim() !== '');
   const [headers, ...rows] = lines;
