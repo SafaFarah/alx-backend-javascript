774cb1a076b7c12b84195aa961be0b62f504ef26
SafaFarah <kangjisook13@gmail.com>
2024-09-20 21:23:14 +0300

    create 0-calcul.js

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', function() {
+  it('should return 4 when 1 and 3 are passed', function() {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('should return 5 when 1 and 3.7 are passed (rounded)', function() {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should return 5 when 1.2 and 3.7 are passed (rounded)', function() {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should return 6 when 1.5 and 3.7 are passed (rounded)', function() {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
