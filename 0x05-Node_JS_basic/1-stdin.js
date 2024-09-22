4062c09026ce34c3cfb4800cc5d125f6b20fb7fd
SafaFarah <kangjisook13@gmail.com>
2024-09-22 22:37:33 +0300

    debug 0x05-Node_JS_basic

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
+  process.stdout.write(`Your name is: ${data.toString()}\n`);
   process.exit();
 });
 process.on('exit', () => {
