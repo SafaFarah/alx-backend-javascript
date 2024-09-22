c71c528d8d99bce0aa2053b9bac92176713b57b8
SafaFarah <kangjisook13@gmail.com>
2024-09-22 20:19:31 +0300

    debug 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,4 +6,8 @@ describe('calculateNumber', function() {
   it('should return 4 when 1 and 3 are passed', function() {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
+
+  it('should return 5 when 1 and 3.7 are passed (round second number)', function() {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
 });
